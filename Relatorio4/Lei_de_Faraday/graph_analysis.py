e9b4c7a975a2d8b338970f771b0fe5a28eee0ae8
Silvio Lacerda <41975339+silvio-lacerda@users.noreply.github.com>
2023-06-15 10:43:19 -0300

    edit

--- Relatorio4/Lei_de_Faraday/graph_analysis.py
+++ Relatorio4/Lei_de_Faraday/graph_analysis.py
@@ -3,10 +3,15 @@ from matplotlib import pyplot
 import numpy as np
 import pandas as pd
 
+TEMPO = "Segundos(s)"
+CANAL1 = "Canal 1(V)"
+CANAL2 = "Canal 2(V)"
+
+
 file = pd.read_csv('data/1152.csv', sep=",")
 
-x = list(float(b) for b in list(file["second"]))
-y = list(float(b) for b in list(file["Volt1"]))
+x = list(float(b) for b in list(file[TEMPO]))
+y = list(float(b) for b in list(file[CANAL1]))
 
 pyplot.plot(x, y, 'r-', label='data')
 pyplot.xlabel("Tempo (s)")
